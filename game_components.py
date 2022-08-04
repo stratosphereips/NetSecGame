d1044a82478f80e3224f19baeb23cbede15dc43b
Ondrej Lukas <ondrej.lukas95@gmail.com>
2022-08-04 16:39:58 +0200

    Exended GameState class

--- game_components.py
+++ game_components.py
@@ -1,4 +1,5 @@
 #Author Ondrej Lukas - ondrej.lukas@aic.fel.cvut.cz
+from asyncio import protocols
 from collections import namedtuple
 
 #Transition between nodes
@@ -12,7 +13,6 @@ Actions = [
     Transition(3, "FindData",0.5,0.9,2,0.1),
     Transition(4, "ExecuteCodeInService",0.3,0.3,0.1,0.3),
     Transition(5, "ExfiltrateData",0.8,0.8,1000,0.1),
-    Transition(6, "LeaveHost",None,None,None,None)
 ]
 
 """
@@ -21,15 +21,33 @@ Game state represents the states in the game state space.
 """
 class GameState(object):
     def __init__(self, id:float, controled_hosts:list=[], known_hosts:list=[], know_services:dict={}, defender_nodes:list=[]) -> None:
-        self.id = id
-        self.controled_hosts = controled_hosts
-        self.known_hosts = known_hosts
-        self.know_services = self.know_services
-        self.defender_nodes = defender_nodes
-        self.is_terminal = False
+        self._id = id
+        self._controled_hosts = controled_hosts
+        self._known_hosts = known_hosts
+        self._known_services = self.know_services
+        self._known_data = self.known_data
+        self._defender_nodes = defender_nodes
+        self._is_terminal = False
     
     def str(self) ->str:
-        return "Node {self.id}"
+        return "Node {self._id}"
     
-    def get_actions(self):
-        raise NotImplementedError
+    @property
+    def controled_hosts(self):
+        return self._controled_hosts
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def is_terminal(self):
+        return self._is_terminal
+    
+    @property
+    def known_hosts(self):
+        return self._known_hosts
+    
+    @property
+    def known_services(self):
+        return self._known_services
