e00f16a60471b8e9ff648a7edc962c51ee3a5fc2
Ondrej Lukas <ondrej.lukas95@gmail.com>
2022-08-08 15:44:34 +0200

    Defended nodes moved to the environment from the GameState, Added named tuple for observations

--- game_components.py
+++ game_components.py
@@ -3,38 +3,44 @@ from asyncio import protocols
 from collections import namedtuple
 
 #Transition between nodes
-Transition = namedtuple("Transition", ["id", "action", "default_succes_p", "default_detection_p", "default_reward", "default_cost"])
+Transition = namedtuple("Transition", ["type", "default_succes_p", "default_detection_p", "default_reward", "default_cost"])
+
+#Action
+Action = namedtuple("Action", ["transition", "parameters"])
+
+#Observation - given to agent after taking an action
+Observation = namedtuple("Observation", ["observation", "rewards", "is_terminal", "done", "info"])
 
 #List of transitions available for attacker with default parameters
-Actions = [
-    Transition(0, "Wait", 1,0,0,0),
-    Transition(1, "ScanNetwork",0.9,0.5,1,0.1),
-    Transition(2, "FindServices",0.9,0.6,1,0.1),
-    Transition(3, "FindData",0.5,0.9,2,0.1),
-    Transition(4, "ExecuteCodeInService",0.3,0.3,0.1,0.3),
-    Transition(5, "ExfiltrateData",0.8,0.8,1000,0.1),
-]
+transitions = {
+    "ScanNetwork": Transition("ScanNetwork",0.9,0.5,1,0.1),
+    "FindServices": Transition("FindServices",0.9,0.6,1,0.1),
+    "FindData": Transition("FindData",0.5,0.9,2,0.1),
+    "ExecuteCodeInService": Transition("ExecuteCodeInService",0.3,0.3,0.1,0.3),
+    "ExfiltrateData": Transition("ExfiltrateData",0.8,0.8,1000,0.1),
+}
 
 """
 Game state represents the states in the game state space.
 
 """
 class GameState(object):
-    def __init__(self, id:float, controled_hosts:list=[], known_hosts:list=[], know_services:dict={}, defender_nodes:list=[]) -> None:
+    def __init__(self, id:float, controlled_hosts:list=[], known_hosts:list=[], know_services:dict={},
+    known_data:dict={}, known_networks=[], defender_nodes:list=[]) -> None:
         self._id = id
-        self._controled_hosts = controled_hosts
+        self._controlled_hosts = controlled_hosts
+        self._known_networks = known_networks
         self._known_hosts = known_hosts
-        self._known_services = self.know_services
-        self._known_data = self.known_data
-        self._defender_nodes = defender_nodes
+        self._known_services = know_services
+        self._known_data = known_data
         self._is_terminal = False
     
     def str(self) ->str:
         return "Node {self._id}"
     
     @property
-    def controled_hosts(self):
-        return self._controled_hosts
+    def controlled_hosts(self):
+        return self._controlled_hosts
     
     @property
     def id(self):
@@ -51,3 +57,7 @@ class GameState(object):
     @property
     def known_services(self):
         return self._known_services
+    
+    @property
+    def known_networks(self):
+        return self._known_networks
