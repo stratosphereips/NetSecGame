10127467daf8bfac49cb270d76a0bf79294b4bad
Sebas Garcia <eldraco@gmail.com>
2023-02-23 14:57:16 +0100

    By default eval every 50 episodes

--- double_q_agent.py
+++ double_q_agent.py
@@ -125,7 +125,7 @@ if __name__ == '__main__':
     parser.add_argument("--defender", help="Is defender present", default=False, action="store_true")
     parser.add_argument("--scenario", help="Which scenario to run in", default="scenario1", type=str)
     parser.add_argument("--evaluate", help="Do not train, only run evaluation", default=False, action="store_true")
-    parser.add_argument("--eval_each", help="Sets periodic evaluation during training", default=500, type=int)
+    parser.add_argument("--eval_each", help="Sets periodic evaluation during training", default=50, type=int)
     parser.add_argument("--eval_for", help="Sets evaluation length", default=1000, type=int)
     parser.add_argument("--random_start", help="Sets if starting position and goal data is randomized", default=False, action="store_true")
     parser.add_argument("--seed", help="Sets the random seed", type=int, default=42)
