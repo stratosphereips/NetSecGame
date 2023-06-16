d95542145df81cf9c7b811ab8c75816896881fb7
Ondrej Lukas <ondrej.lukas95@gmail.com>
2023-06-16 11:45:57 +0200

    Remove wildard import

--- env/scenarios/smaller_scenario_configuration.py
+++ env/scenarios/smaller_scenario_configuration.py
@@ -1,6 +1,5 @@
 # This file defines the hosts and their characteristics, the services they run, the users they have and their security levels, the data they have, and in the router/FW all the rules of which host can access what
 import cyst.api.configuration as cyst_cfg
-#from cyst.api.configuration import *
 from cyst.api.configuration.network.elements import RouteConfig
 from cyst.api.logic.access import AuthenticationProviderType, AuthenticationTokenType, AuthenticationTokenSecurity
 
