04bf96d19af79faa4204f35c9f36f0ebfe46f8ae
mrigaki <mrigaki@gmail.com>
2023-07-19 15:48:53 +0200

    LLM QA. Add metrics and memory buffer size

--- agents/llm_qa/llm_agent_qa.py
+++ agents/llm_qa/llm_agent_qa.py
@@ -25,6 +25,9 @@ local_services = ['can_attack_start_here']
 
 # Set the logging
 import logging
+from torch.utils.tensorboard import SummaryWriter
+import time
+import numpy as np
 
 ACTION_MAPPER = {
     "ScanNetwork": ActionType.ScanNetwork,
@@ -81,8 +84,8 @@ Once you have known data exfiltrate them to the target one by one.
 Q1 = "List the objects in the current status and the actions they can be used. Be specific."
 Q2 = "List the top 3 sub-tasks you should follow with specific parameters. Indicate their priority out of 5."
 Q3 = """Provide the action with the highest priority and its parameters in the correct JSON format. Do not repeat past actions.
-Action: 
-"""
+Action: """
+Q4 = """Action: """
 
 def validate_action_in_state(llm_response, state):
     """Check the LLM response and validate it against the current state."""
@@ -254,109 +257,208 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--task_config_file", help="Reads the task definition from a configuration file", default=path.join(path.dirname(__file__), 'netsecenv-task.yaml'), action='store', required=False)
     parser.add_argument("--llm", type=str, choices=["gpt-4", "gpt-3.5-turbo", "gpt-3.5-turbo-16k"], default="gpt-3.5-turbo", help="LLM used with OpenAI API")
+    parser.add_argument("--test_episodes", help="Number of test episodes to run", default=30, action='store', required=False, type=int)
+    parser.add_argument("--memory_buffer", help="Number of actions to remember and pass to the LLM", default=5, action='store', required=False, type=int)
     args = parser.parse_args()
 
     logger = logging.getLogger('llm_qa')
 
     env = NetworkSecurityEnvironment(args.task_config_file)
-    # Initialize the game
-    observation = env.reset()
-    current_state = observation.state
+    # Setup tensorboard
+    run_name = f"netsecgame__llm_qa__{env.seed}__{int(time.time())}"
+    writer = SummaryWriter(f"agents/llm_qa/logs/{run_name}")
 
-    num_iterations = 100
-    taken_action = None
-    memories = []
-    total_reward = 0
-    num_actions = 0
+    # Run multiple episodes to compute statistics
+    wins = 0
+    detected = 0
+    reach_max_steps = 0
+    returns = []
+    num_steps = []
+    num_win_steps = []
+    num_detected_steps = []
+    num_actions_repeated = []
+    reward_memory = ''
+    # We are still not using this, but we keep track
+    is_detected = False
 
-    # Populate the instructions based on the pre-defined goal
-    goal = copy.deepcopy(env._win_conditions)
-    jinja_environment = jinja2.Environment()
-    template = jinja_environment.from_string(INSTRUCTIONS_TEMPLATE)
-    target_host = list(goal["known_data"].keys())[0]
-    data = goal["known_data"][target_host].pop()
-    instructions = template.render(user=data.owner, data=data.id, target_host=target_host)
+    # Control to save the 1st prompt in tensorboard
+    save_first_prompt = False
 
-    for i in range(num_iterations):
-        good_action = False
+    for episode in range(1, args.test_episodes + 1):
+        actions_took_in_episode = []
 
-        # Step 1
-        status_prompt = create_status_from_state(observation.state)
-        messages = [
-            {"role": "user", "content": instructions},
-            {"role": "user", "content": status_prompt},
-            {"role": "user", "content": Q1}
-        ]
-        response = openai_query(messages, max_tokens=1024, model=args.llm)
-        print("LLM (step 1): %s", response)
+        logger.info(f'Running episode {episode}')
+        print(f'Running episode {episode}')
 
-        # Step 2
-        memory_prompt = create_mem_prompt(memories)
-        # messages = [
-        #     {"role": "user", "content": instructions},
-        #     {"role": "user", "content": status_prompt},
-        #     {"role": "user", "content": COT_PROMPT2},
-        #     {"role": "user", "content": response},
-        #     {"role": "user", "content": memory_prompt},
-        #     {"role": "user", "content": Q2}
-        # ]
+        # Initialize the game
+        observation = env.reset()
+        current_state = observation.state
 
-        # response = openai_query(messages, max_tokens=1024, model="gpt-4")
-        # print("LLM (step 2): %s", response)
+        num_iterations = 100
+        taken_action = None
+        memories = []
+        total_reward = 0
+        num_actions = 0
+        repeated_actions = 0
 
-        # Step 3
-        messages = [
-            {"role": "user", "content": instructions},
-            {"role": "user", "content": status_prompt},
-            {"role": "user", "content": COT_PROMPT2},
-            {"role": "user", "content": response},
-            {"role": "user", "content": memory_prompt},
-            {"role": "user", "content": Q3}
-        ]
+        # Populate the instructions based on the pre-defined goal
+        goal = copy.deepcopy(env._win_conditions)
+        jinja_environment = jinja2.Environment()
+        template = jinja_environment.from_string(INSTRUCTIONS_TEMPLATE)
+        target_host = list(goal["known_data"].keys())[0]
+        data = goal["known_data"][target_host].pop()
+        instructions = template.render(user=data.owner, data=data.id, target_host=target_host)
 
-        # print(messages)
-        response = openai_query(messages, max_tokens=80, model=args.llm)
+        for i in range(num_iterations):
+            good_action = False
 
-        print(f"LLM (step 3): {response}")
-        logger.info("LLM (step 3): %s", response)
+            # Step 1
+            status_prompt = create_status_from_state(observation.state)
+            messages = [
+                {"role": "user", "content": instructions},
+                {"role": "user", "content": status_prompt},
+                {"role": "user", "content": Q1}
+            ]
+            response = openai_query(messages, max_tokens=1024, model=args.llm)
+            logger.info("LLM (step 1): %s", response)
 
-        try:
-            if response.startswith("Action: "):
-                response = response[8:]
-            response = eval(response)
-            # Validate action based on current states
-            is_valid, action = create_action_from_response(response, observation.state)
-        except SyntaxError:
-            print("Eval failed")
-            is_valid = False
+            # Step 2
+            memory_prompt = create_mem_prompt(memories[-args.memory_buffer:])
+            # messages = [
+            #     {"role": "user", "content": instructions},
+            #     {"role": "user", "content": status_prompt},
+            #     {"role": "user", "content": COT_PROMPT2},
+            #     {"role": "user", "content": response},
+            #     {"role": "user", "content": memory_prompt},
+            #     {"role": "user", "content": Q2}
+            # ]
 
-        if is_valid:
-            observation = env.step(action)
-            taken_action = action
-            total_reward += observation.reward
+            # response = openai_query(messages, max_tokens=1024, model="gpt-4")
+            # logger.info("LLM (step 2): %s", response)
 
-            if observation.state != current_state:
-                good_action = True
-                current_state = observation.state
+            # Step 3
+            messages = [
+                {"role": "user", "content": instructions},
+                {"role": "user", "content": status_prompt},
+                {"role": "user", "content": COT_PROMPT2},
+                {"role": "user", "content": response},
+                {"role": "user", "content": memory_prompt},
+                {"role": "user", "content": Q3}
+            ]
 
-        logger.info(f"Iteration: {i}. Is action valid: {is_valid}, is action good: {good_action}")
-        if observation.done:
-            break
+            # Store the first prompt in tensorboard
+            if not save_first_prompt:
+                writer.add_text('prompt', f'{messages}')
+                save_first_prompt = True
 
-        try:
-            if not is_valid:
-                memories.append((response["action"], response["parameters"], "This action was not valid based on your status."))
-            else:
-                # This is based on the assumption that more valid actions in the state are better/more helpful.
-                # But we could a manual evaluation based on the prior knowledge and weight the different components.
-                # For example: finding new data is better than discovering hosts (?)
-                if good_action:
-                    memories.append((response["action"], response["parameters"], "This action was helpful."))
+            # Query the LLM
+            response = openai_query(messages, max_tokens=80, model=args.llm)
+
+            print(f"LLM (step 3): {response}")
+            logger.info("LLM (step 3): %s", response)
+
+            try:
+                if response.startswith("Action: "):
+                    response = response[8:]
+                response = eval(response)
+                # Validate action based on current states
+                is_valid, action = create_action_from_response(response, observation.state)
+            except SyntaxError:
+                print("Eval failed")
+                is_valid = False
+
+            if is_valid:
+                observation = env.step(action)
+                taken_action = action
+                total_reward += observation.reward
+
+                if observation.state != current_state:
+                    good_action = True
+                    current_state = observation.state
+
+            logger.info(f"Iteration: {i}. Is action valid: {is_valid}, is action good: {good_action}")
+            if observation.done:
+                reason = observation.info
+
+                win = 0
+                # is_detected if boolean
+                is_detected = env.detected
+                steps = env.timestamp
+                epi_return = observation.reward
+                num_actions_repeated += [repeated_actions]
+                if 'goal_reached' in reason['end_reason']:
+                    wins += 1
+                    num_win_steps += [steps]
+                    type_of_end = 'win'
+                elif 'detected' in reason['end_reason']:
+                    detected += 1
+                    num_detected_steps += [steps]
+                    type_of_end = 'detection'
                 else:
-                    memories.append((response["action"], response["parameters"], "This action was not helpful."))
-        except TypeError:
-            # if the LLM sends a response that is not properly formatted.
-            memories.append(f"Response '{response}' was badly formatted.")
+                    num_win_steps += [0]
+                    num_detected_steps += [0]
+                    reach_max_steps += 1
+                    type_of_end = 'max_steps'
+                returns += [epi_return]
+                num_steps += [steps]
 
-logger.info("Total reward: %s", str(total_reward))
-print(f"Total reward: {total_reward}")
+                logger.info(f"\tEpisode {episode} of game ended after {steps} steps. Reason: {reason}. Last reward: {epi_return}")
+                print(f"\tEpisode {episode} of game ended after {steps} steps. Reason: {reason}. Last reward: {epi_return}")
+                break
+
+            try:
+                if not is_valid:
+                    memories.append((response["action"], response["parameters"], "This action was not valid based on your status."))
+                else:
+                    # This is based on the assumption that more valid actions in the state are better/more helpful.
+                    # But we could a manual evaluation based on the prior knowledge and weight the different components.
+                    # For example: finding new data is better than discovering hosts (?)
+                    if good_action:
+                        memories.append((response["action"], response["parameters"], "This action was helpful."))
+                    else:
+                        memories.append((response["action"], response["parameters"], "This action was not helpful."))
+                    
+                    # If the action was repeated count it                    
+                    if action in actions_took_in_episode:
+                        repeated_actions += 1
+
+                    # Store action in memory of all actions so far 
+                    actions_took_in_episode.append(action)
+            except TypeError:
+                # if the LLM sends a response that is not properly formatted.
+                memories.append(f"Response '{response}' was badly formatted.")
+
+    # After all episodes are done. Compute statistics
+    test_win_rate = (wins/(args.test_episodes))*100
+    test_detection_rate = (detected/(args.test_episodes))*100
+    test_max_steps_rate = (reach_max_steps/(args.test_episodes))*100
+    test_average_returns = np.mean(returns)
+    test_std_returns = np.std(returns)
+    test_average_episode_steps = np.mean(num_steps)
+    test_std_episode_steps = np.std(num_steps)
+    test_average_win_steps = np.mean(num_win_steps)
+    test_std_win_steps = np.std(num_win_steps)
+    test_average_detected_steps = np.mean(num_detected_steps)
+    test_std_detected_steps = np.std(num_detected_steps)
+    # Store in tensorboard
+    tensorboard_dict = {"charts/test_avg_win_rate": test_win_rate, "charts/test_avg_detection_rate": test_detection_rate, "charts/test_avg_max_steps_rate": test_max_steps_rate, "charts/test_avg_returns": test_average_returns, "charts/test_std_returns": test_std_returns, "charts/test_avg_episode_steps": test_average_episode_steps, "charts/test_std_episode_steps": test_std_episode_steps, "charts/test_avg_win_steps": test_average_win_steps, "charts/test_std_win_steps": test_std_win_steps, "charts/test_avg_detected_steps": test_average_detected_steps, "charts/test_std_detected_steps": test_std_detected_steps}
+
+    text = f'''Final test after {args.test_episodes} episodes
+        Wins={wins},
+        Detections={detected},
+        winrate={test_win_rate:.3f}%,
+        detection_rate={test_detection_rate:.3f}%,
+        max_steps_rate={test_max_steps_rate:.3f}%,
+        average_returns={test_average_returns:.3f} +- {test_std_returns:.3f},
+        average_episode_steps={test_average_episode_steps:.3f} +- {test_std_episode_steps:.3f},
+        average_win_steps={test_average_win_steps:.3f} +- {test_std_win_steps:.3f},
+        average_detected_steps={test_average_detected_steps:.3f} +- {test_std_detected_steps:.3f}'''
+
+    # Text that is going to be added to the tensorboard. Put any description you want
+
+    experiment_description = "LLM QA agent. " + f"Model: {args.llm}" + f"Conf: {args.task_config_file}" + f"Max steps: {env._max_steps}" + f"Seed: {env._seed}"
+
+    writer.add_text("Description", experiment_description)
+    writer.add_hparams(vars(args), tensorboard_dict)
+    print(text)
+    logger.info(text)
