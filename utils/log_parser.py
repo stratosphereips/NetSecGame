Log parser. Fix calculation of detection according to the env

utils/log_parser.py
@@ -16,7 +16,7 @@ def calculate_episode_lengths(step_sequence):
         if step_seq[i] == 0:
             # If the step is zero then it's a new episode
             # Take the last value as episode length
-            episode_lengths.append(step_sequence[i-1]+1)
+            episode_lengths.append(step_sequence[i - 1] + 1)
     # For the last episode take the latest step value
     episode_lengths.append(step_sequence[-1] + 1)
     return episode_lengths
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     latest_step = 0
 
     for i, line in enumerate(data):
-        # Check if the episode starts and initialize the counters
+        # Check if a new episode starts and if yes initialize the variables
         if start_str in line:
             episode_starts.append(i)
             stop_until_new = False
@@ -74,7 +74,6 @@ if __name__ == '__main__':
             if add_max_reward:
                 rewards.append(-args.max_steps)
             add_max_reward = False
-
 
         # Check if the line contains the env step
         if step_str in line:
@@ -101,7 +100,7 @@ if __name__ == '__main__':
         if detected_str in line and not stop_until_new:
             detected_lengths.append(step_seq[-1] + 1)
             total_detected += 1
-            rewards.append(-args.max_steps)
+            rewards.append(-50-(latest_step+1))
             add_max_reward = False
             stop_until_new = True
 
@@ -109,24 +108,25 @@ if __name__ == '__main__':
         if not stop_until_new:
             stop_until_new = reached_limit(latest_step + 1, args.max_steps, invalid)
             if stop_until_new:
-                # print("here")
-                # rewards.append(-args.max_steps)
+                # Add the negative reward at the start of the next episode
+                # This is needed because in the last step the agent may win
+                # and this takes precedence over the max steps calculation
                 add_max_reward = True
 
     # If the last run reached the max steps add it here
     if add_max_reward:
         rewards.append(-args.max_steps)
 
+    episode_lengths = calculate_episode_lengths(step_seq)
+    assert len(rewards) == len(episode_starts)
+    assert len(episode_lengths) == len(episode_starts)
+
     print(f"Episodes: {len(episode_starts)}")
     print(f"Wins: {total_wins}")
+    print(f"Detections: {total_detected}")
     print(f"Win rate: {100*total_wins/len(episode_starts):.3f}%")
-    print(f"Detected: {total_detected}")
     print(f"Detection rate: {100*total_detected/len(episode_starts):.3f}%")
     print(f"Average rewards: {np.mean(rewards):.3f} +- {np.std(rewards):.3f}")
-
-    episode_lengths = calculate_episode_lengths(step_seq)
     print(f"Average episode length: {np.mean(episode_lengths):.3f} +- {np.std(episode_lengths):.3f}")
     print(f"Average win length: {np.mean(win_lengths):.3f} +- {np.std(win_lengths):.3f}")
     print(f"Average detected length: {np.mean(detected_lengths):.3f} +- {np.std(detected_lengths):.3f}")
-    assert len(rewards) == len(episode_starts)
-    assert len(episode_lengths) == len(episode_starts)