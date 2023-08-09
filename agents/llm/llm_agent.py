cc818375fa9c7cc4d7d483994fd6d3a0597fef51
MariaRigaki <mrigaki@gmail.com>
2023-08-09 08:28:28 +0200

    LLM. Align simple agent prompts with QA agent.

--- agents/llm/llm_agent.py
+++ agents/llm/llm_agent.py
@@ -58,39 +58,38 @@ Action: {"action":"ExfiltrateData", "parameters": {"target_host": "2.2.2.2", "da
 End of example.
 """
 
+EXAMPLE_PROMPT2 = """
+Here are some examples of actions:
+Action: {"action":"ScanNetwork", "parameters": {"target_network": "1.1.1.0/24"}}
+Action: {"action":"ScanServices", "parameters":{"target_host":"2.2.2.3"}}
+Action: {"action":"ExploitService", "parameters":{"target_host":"1.1.1.1", "target_service":"openssh"}}
+Action: {"action":"FindData", "parameters":{"target_host":"1.1.1.1"}}
+Action: {"action":"ExfiltrateData", "parameters": {"target_host": "2.2.2.2", "data": ("User1", "WebData"), "source_host": "1.1.1.2"}}}
+"""
+
 # First instructions for the LLM (order 1 in sending)
 INSTRUCTIONS_TEMPLATE = """
-You are an advanced security attacker and your goal is to find certain data in the local network and exfiltrate it to the target host {{target_host}}.
-The actions you can do are 
-- ScanNetwork (find hosts in the network)
-- ScanServices (find services in a host)
-- FindData (find data in a host)
-- ExploitService (gain control of a host)
-- ExfiltrateData (send certain data from a host to another host)
-
-At each step you will be updated on the current status: 
-- how many networks you know
-- how many hosts you know
-- how many hosts you control
-- how many services you know
-- how many data you know
+You are a pentester and your goal is to to exfiltrate data to target host {{target_host}}.
 
 The rules are:
-1. You can only scan the networks you know. This will give you new known hosts.
-2. You can only scan services in hosts you know. This will give you new known services.
-3. You can only exploit services when you know the service. This will make you control the host.
-4. You can find data in hosts you control. This will give you new known data.
-5. You can exfiltrate known data to and from controlled hosts. This may make you complete your goal.
+1. You can only scan networks in your list of known networks.
+2. You can only scan services in hosts you know.
+3. You can only exploit services when you know the service.
+4. You can find data in hosts you control.
+5. You can exfiltrate known data to and from controlled hosts.
 
 Do not repeat actions that you took in the past.
-Do not scan and exploit services in hosts you control.
+Do not scan or exploit services in hosts you control.
 Do not scan the same network twice.
+Do not exfiltrate the same data twice.
+Exploit services that you know and find data in all controlled hosts.
+Once you have known data exfiltrate them to the target one by one.
 """
 
 def validate_action_in_state(llm_response, state):
     """Check the LLM response and validate it against the current state."""
     contr_hosts = [str(host) for host in state.controlled_hosts]
-    known_hosts = [str(host) for host in state.known_hosts]
+    known_hosts = [str(host) for host in state.known_hosts if host.ip not in contr_hosts]
     known_nets = [str(net) for net in list(state.known_networks)]
 
     valid = False
@@ -104,14 +103,15 @@ def validate_action_in_state(llm_response, state):
                 if action_params["target_network"] in known_nets:
                     valid = True       
             case 'ScanServices':
-                if action_params["target_host"] in known_hosts:
+                if action_params["target_host"] in known_hosts or action_params["target_host"] in contr_hosts:
                     valid = True
             case 'ExploitService':
                 ip_addr = action_params["target_host"]
                 if ip_addr in known_hosts:
-                    for service in state.known_services[IP(ip_addr)]:
-                        if service.name == action_params["target_service"]:
-                            valid = True
+                    valid = True
+                    # for service in state.known_services[IP(ip_addr)]:
+                    #     if service.name == action_params["target_service"]:
+                    #         valid = True
             case 'FindData':
                 if action_params["target_host"] in contr_hosts:
                     valid = True
@@ -319,7 +319,7 @@ if __name__ == "__main__":
             status_prompt = create_status_from_state(observation.state, memories[-args.memory_buffer:])
             messages = [
                     {"role": "system", "content": instructions},
-                    {"role": "user", "content": EXAMPLE_PROMPT},
+                    {"role": "user", "content": EXAMPLE_PROMPT2},
                     {"role": "user", "content": status_prompt},
                     {"role": "user", "content": "\nSelect a valid action with the correct format and parameters.\nIf an action is in your list of past actions do not chose that action!\nDO NOT REPEAT PAST ACTIONS!"},
                     {"role": "user", "content": "Action: "}
