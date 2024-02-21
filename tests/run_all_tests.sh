8c4cafa8587bff9ebc6d40b11584e97cdf2c14a6
Sebastian Garcia <eldraco@gmail.com>
2024-02-21 14:30:01 +0100

    test. Change python to python3

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -3,7 +3,7 @@
 # run all unit tests, -n *5 means distribute tests on 5 different process
 # -s to see print statements as they are executed
 
-python  -m pytest tests/test_actions.py -p no:warnings -vvvv -s --full-trace
-python  -m pytest tests/test_components.py -p no:warnings -vvvv -s --full-trace
-python  -m pytest tests/test_coordinator.py -p no:warnings -vvvv -s --full-trace
+python3  -m pytest tests/test_actions.py -p no:warnings -vvvv -s --full-trace
+python3  -m pytest tests/test_components.py -p no:warnings -vvvv -s --full-trace
+python3  -m pytest tests/test_coordinator.py -p no:warnings -vvvv -s --full-trace
 
