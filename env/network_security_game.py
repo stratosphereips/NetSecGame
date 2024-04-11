d5af89a562114ac18ca58e88dc9580a91536875f
Ondrej Lukas <ondrej.lukas95@gmail.com>
2024-04-11 14:41:39 +0200

    Added FW check for FindServices

--- env/network_security_game.py
+++ env/network_security_game.py
@@ -832,16 +832,19 @@ class NetworkSecurityEnvironment(object):
         next_nets, next_known_h, next_controlled_h, next_services, next_data = self._state_parts_deep_copy(current)
         logger.info(f"\t\tSearching for services in {action.parameters['target_host']}")
         if "source_host" in action.parameters.keys() and action.parameters["source_host"] in current.controlled_hosts:
-            found_services = self._get_services_from_host(action.parameters["target_host"], current.controlled_hosts)
-            logger.info(f"\t\t\tFound {len(found_services)}: {found_services}")
-            if len(found_services) > 0:
-                next_services[action.parameters["target_host"]] = found_services
+            if self._firewall_check(action.parameters["source_host"], action.parameters['target_host']):
+                found_services = self._get_services_from_host(action.parameters["target_host"], current.controlled_hosts)
+                logger.info(f"\t\t\tFound {len(found_services)}: {found_services}")
+                if len(found_services) > 0:
+                    next_services[action.parameters["target_host"]] = found_services
 
-                #if host was not known, add it to the known_hosts ONLY if there are some found services
-                if action.parameters["target_host"] not in next_known_h:
-                    logger.info(f"\t\tAdding {action.parameters['target_host']} to known_hosts")
-                    next_known_h.add(action.parameters["target_host"])
-                    next_nets = next_nets.union({net for net, values in self._networks.items() if action.parameters["target_host"] in values})
+                    #if host was not known, add it to the known_hosts ONLY if there are some found services
+                    if action.parameters["target_host"] not in next_known_h:
+                        logger.info(f"\t\tAdding {action.parameters['target_host']} to known_hosts")
+                        next_known_h.add(action.parameters["target_host"])
+                        next_nets = next_nets.union({net for net, values in self._networks.items() if action.parameters["target_host"] in values})
+            else:
+                logger.info(f"\t\t\tConnection {action.parameters['source_host']} -> {action.parameters['target_host']} blocked by FW. Skipping")
         else:
             logger.info(f"\t\t\t Invalid source_host:'{action.parameters['source_host']}'")
         return components.GameState(next_controlled_h, next_known_h, next_services, next_data, next_nets)
