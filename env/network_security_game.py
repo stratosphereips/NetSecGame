0487fdd400b360d56a4f1ceb8020248787cd28ea
Sebastian Garcia <eldraco@gmail.com>
2024-04-11 11:38:03 +0200

    netsecgame. Fix detection that was broken due to deleted line

--- env/network_security_game.py
+++ env/network_security_game.py
@@ -1087,6 +1087,7 @@ class NetworkSecurityEnvironment(object):
 
             # 1. Perform the action
             if random.random() <= action.type.default_success_p or action_type == 'realworld':
+                self._actions_played.append(action)
                 next_state = self._execute_action(self._current_state, action, action_type=action_type)
             else:
                 logger.info("\tAction NOT sucessful")
