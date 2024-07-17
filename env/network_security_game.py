bd61fc0740b9975fad33b1699f48d79d98bbc9b3
Sebastian Garcia <eldraco@gmail.com>
2024-07-17 11:50:24 +0200

    netsecenv. Delete old reset() for the defender that does not exist anymore

--- env/network_security_game.py
+++ env/network_security_game.py
@@ -988,7 +988,6 @@ class NetworkSecurityEnvironment(object):
       
 
         self._actions_played = []
-        self._defender.reset()
 
     def step(self, state:components.GameState, action:components.Action, agent_id:tuple,action_type='netsecenv')-> components.GameState:
         """
