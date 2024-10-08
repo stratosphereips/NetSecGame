08c472d5dac09ecdcfde535f46739957d456fc7a
Ondrej Lukas <ondrej.lukas95@gmail.com>
2024-10-08 17:09:25 +0200

    Move aidojo_world to separate file

--- env/network_security_game.py
+++ env/network_security_game.py
@@ -9,48 +9,12 @@ import itertools
 import copy
 from cyst.api.configuration import NodeConfig, RouterConfig, ConnectionConfig, ExploitConfig, FirewallPolicy
 import numpy as np
-import logging
 from faker import Faker
-from utils.utils import ConfigParser
+from env.aidojo_world import AIDojoWorld
 import subprocess
 import xml.etree.ElementTree as ElementTree
 
-class AIDojoWorld(object):
-
-    def __init__(self, task_config_file:str, world_name:str)->None:
-        self.task_config = ConfigParser(task_config_file)
-        self.logger = logging.getLogger(world_name)
-
-    def step(current_state:components.GameState, action:components.Action, agent_id:tuple)-> components.GameState:
-        """
-        Executes given action in a current state of the environment and produces new GameState.
-        """
-        raise NotImplementedError
-
-    def create_state_from_view(self, view:dict, add_neighboring_nets:bool=True)->components.GameState:
-        """
-        Produces a GameState based on the view of the world.
-        """
-        raise NotImplementedError
-    
-    def reset()->None:
-        """
-        Resets the world to its initial state.
-        """
-        raise NotImplementedError
-
-    def update_goal_descriptions(self, goal_description:str)->str:
-       """
-       Takes the existing goal description (text) and updates it with respect to the world.
-       """
-       raise NotImplementedError
-    
-    def update_goal_dict(self, goal_dict:dict)->dict:
-        """
-        Takes the existing goal dict and updates it with respect to the world.
-        """
-        raise NotImplementedError
-    
+ 
 
 class NetworkSecurityEnvironment(AIDojoWorld):
     """
