ac70b6244c1550d507c33e54c7e7be53cc4d2d98
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-02-06 10:37:45 +0100

    add tests

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -6,7 +6,7 @@
 #python3  -m pytest tests/test_actions.py -p no:warnings -vvvv -s --full-trace
 python3  -m pytest tests/test_components.py -p no:warnings -vvvv -s --full-trace
 python3  -m pytest tests/test_game_coordinator.py -p no:warnings -vvvv -s --full-trace
-# Coordinator tesst
+python3  -m pytest tests/test_global_defender.py -p no:warnings -vvvv -s --full-trace
 #python3  -m pytest tests/test_coordinator.py -p no:warnings -vvvv -s --full-trace
 
 # run ruff check as well
