0328c0f088cf93623439a45effd46117dfb265fd
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-03-25 11:37:37 +0100

    Make it compatible with Cyst requirements

--- AIDojoCoordinator/worlds/NSEGameCoordinator.py
+++ AIDojoCoordinator/worlds/NSEGameCoordinator.py
@@ -97,15 +97,15 @@ class NSGCoordinator(GameCoordinator):
             for controlled_host in controlled_hosts:
                 for net in self._get_networks_from_host(controlled_host): #TODO
                     net_obj = netaddr.IPNetwork(str(net))
-                    if net_obj.ip.is_ipv4_private_use(): #TODO
+                    if net_obj.ip.is_private(): #TODO
                         known_networks.add(net)
                         net_obj.value += 256
-                        if net_obj.ip.is_ipv4_private_use():
+                        if net_obj.ip.is_private():
                             ip = Network(str(net_obj.ip), net_obj.prefixlen)
                             self.logger.debug(f'\tAdding {ip} to agent')
                             known_networks.add(ip)
                         net_obj.value -= 2*256
-                        if net_obj.ip.is_ipv4_private_use():
+                        if net_obj.ip.is_private():
                             ip = Network(str(net_obj.ip), net_obj.prefixlen)
                             self.logger.debug(f'\tAdding {ip} to agent')
                             known_networks.add(ip)
@@ -185,6 +185,7 @@ class NSGCoordinator(GameCoordinator):
                         if node_obj.id not in self._data:
                             self._data[node_obj.id] = set()
                         datapoint = Data(data.owner, data.description)
+                        self.logger.info(f"\t\t\t\tAddinng {datapoint}")
                         self._data[node_obj.id].add(datapoint)
                         # add content
                         self._data_content[node_obj.id, datapoint.id] = f"Content of {datapoint.id}"
@@ -232,7 +233,7 @@ class NSGCoordinator(GameCoordinator):
                 # LOCAL NETWORKS
                 for net, ips in self._networks.items():
                     # IF net is local, allow connection between all nodes in it
-                    if netaddr.IPNetwork(str(net)).ip.is_ipv4_private_use():
+                    if netaddr.IPNetwork(str(net)).ip.is_private():
                         for src in ips:
                             for dst in ips:
                                 firewall[src].add(dst)
@@ -240,9 +241,9 @@ class NSGCoordinator(GameCoordinator):
                 # LOCAL TO INTERNET
                 for net, ips in self._networks.items():
                     # IF net is local, allow connection between all nodes in it
-                    if netaddr.IPNetwork(str(net)).ip.is_ipv4_private_use():
+                    if netaddr.IPNetwork(str(net)).ip.is_private():
                         for public_net, public_ips in self._networks.items():
-                            if not netaddr.IPNetwork(str(public_net)).ip.is_ipv4_private_use():
+                            if not netaddr.IPNetwork(str(public_net)).ip.is_private():
                                 for src in ips:
                                     for dst in public_ips:
                                         firewall[src].add(dst)
@@ -302,7 +303,7 @@ class NSGCoordinator(GameCoordinator):
         # generate mapping for networks
         private_nets = []
         for net in self._networks.keys():
-            if netaddr.IPNetwork(str(net)).ip.is_ipv4_private_use():
+            if netaddr.IPNetwork(str(net)).ip.is_private():
                 private_nets.append(net)
             else:
                 mapping_nets[net] = Network(fake.ipv4_public(), net.mask)
@@ -326,7 +327,7 @@ class NSGCoordinator(GameCoordinator):
                     # find the new mapping 
                     new_net_addr = netaddr.IPNetwork(str(mapping_nets[private_nets_sorted[0]])).ip + diff_ip
                     # evaluate if its still a private network
-                    is_private_net_checks.append(new_net_addr.is_ipv4_private_use())
+                    is_private_net_checks.append(new_net_addr.is_private())
                     # store the new mapping
                     mapping_nets[private_nets_sorted[i]] = Network(str(new_net_addr), private_nets_sorted[i].mask)
                 if False not in is_private_net_checks: # verify that ALL new networks are still in the private ranges
@@ -773,7 +774,7 @@ class NSGCoordinator(GameCoordinator):
     def _get_all_local_ips(self)->set:
         local_ips = set()
         for net, ips in self._networks.items():
-            if netaddr.IPNetwork(str(net)).ip.is_ipv4_private_use():
+            if netaddr.IPNetwork(str(net)).ip.is_private():
                 for ip in ips:
                     local_ips.add(self._ip_mapping[ip])
         self.logger.info(f"\t\t\tLocal ips: {local_ips}")
