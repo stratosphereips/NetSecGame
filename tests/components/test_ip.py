41975657ef3a5069588872bc8b670589eaa74cac
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-06-26 13:53:05 +0200

    Add test ip from dict

--- tests/components/test_ip.py
+++ tests/components/test_ip.py
@@ -36,3 +36,10 @@ class TestComponentIP:
     def test_ip_is_not_private(self):
         ip_1 = IP("192.143.1.15")
         assert ip_1.is_private() is False
+
+    def test_ip_from_dict(self):
+        """Test creating an IP object from a dictionary"""
+        d = {"ip": "10.0.0.1"}
+        ip = IP.from_dict(d)
+        assert isinstance(ip, IP)
+        assert ip.ip == "10.0.0.1"
