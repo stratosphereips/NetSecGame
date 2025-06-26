c5933be5995ebd02ab8ec1ebedb91c9cf28e02b1
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-06-26 14:29:19 +0200

    remove redundant class

--- tests/components/test_ip.py
+++ tests/components/test_ip.py
@@ -1,6 +1,7 @@
 # Authors:  Maria Rigaki - maria.rigaki@aic.fel.cvut.cz
 #           Ondrej Lukas - ondrej.lukas@aic.fel.cvut.cz
 import pytest
+import dataclasses
 from AIDojoCoordinator.game_components import IP
 
 @pytest.fixture
@@ -28,60 +29,59 @@ def sample_public_ip():
     ip_str = "8.8.8.8"
     return IP(ip_str), ip_str
 
-class TestComponentIP:
-    """
-    Tests related to the IP datclass
-    """
-    def test_ip_repr(self, sample_private_ip1):
-        """Test the object representation"""
-        ip_1, ip_str = sample_private_ip1
-        assert repr(ip_1) == ip_str
+def test_ip_repr(sample_private_ip1):
+    """Test the object representation"""
+    ip_1, ip_str = sample_private_ip1
+    assert repr(ip_1) == ip_str
 
-    def test_ip_equal(self, sample_private_ip1, sample_private_ip1_copy):
-        """Test that two IP objects with the same IP are equal"""
-        ip_1, _ = sample_private_ip1
-        ip_2, _ = sample_private_ip1_copy
-        assert ip_1 == ip_2
+def test_ip_equal(sample_private_ip1, sample_private_ip1_copy):
+    """Test that two IP objects with the same IP are equal"""
+    ip_1, _ = sample_private_ip1
+    ip_2, _ = sample_private_ip1_copy
+    assert ip_1 == ip_2
 
-    def test_ip_not_equal(self, sample_private_ip1, sample_private_ip2):
-        """Test that two IP objects with different IPs are not equal"""
-        ip_1, _ = sample_private_ip1
-        ip_2, _ = sample_private_ip2
-        assert ip_1 != ip_2
+def test_ip_not_equal(sample_private_ip1, sample_private_ip2):
+    """Test that two IP objects with different IPs are not equal"""
+    ip_1, _ = sample_private_ip1
+    ip_2, _ = sample_private_ip2
+    assert ip_1 != ip_2
 
-    def test_ip_not_str(self, sample_private_ip1):
-        """Test that the IP object is not equal to a string"""
-        ip_1, _ = sample_private_ip1
-        ip_2 = "192.168.2.15"
-        assert ip_1 != ip_2
+def test_ip_not_str(sample_private_ip1):
+    """Test that the IP object is not equal to a string"""
+    ip_1, _ = sample_private_ip1
+    ip_2 = "192.168.2.15"
+    assert ip_1 != ip_2
 
-    def test_ip_is_private(self, sample_private_ip1):
-        ip_1, _ = sample_private_ip1
-        assert ip_1.is_private() is True
+def test_ip_is_private(sample_private_ip1):
+    ip_1, _ = sample_private_ip1
+    assert ip_1.is_private() is True
 
-    def test_ip_is_not_private(self, sample_public_ip):
-        ip_1, _ = sample_public_ip
-        assert ip_1.is_private() is False
+def test_ip_is_not_private(sample_public_ip):
+    ip_1, _ = sample_public_ip
+    assert ip_1.is_private() is False
 
-    def test_ip_from_dict(self, sample_private_ip1):
-        """Test creating an IP object from a dictionary"""
-        ip_1, ip1_str = sample_private_ip1
-        d = {"ip": ip1_str}
+def test_ip_from_dict(sample_private_ip1):
+    """Test creating an IP object from a dictionary"""
+    ip_1, ip1_str = sample_private_ip1
+    d = dataclasses.asdict(ip_1)
+    assert isinstance(d, dict)
+    assert d["ip"] == ip1_str
+    # Create IP object from dictionary
+    ip = IP.from_dict(d)
+    assert isinstance(ip, IP)
+    assert ip.ip == ip1_str
+    assert ip == ip_1
+
+def test_ip_from_dict_invalid():
+    """Test creating an IP object from an invalid dictionary"""
+    d = {"ip": "invalid_ip"}
+    try:
         ip = IP.from_dict(d)
-        assert isinstance(ip, IP)
-        assert ip.ip == ip1_str
-        assert ip == ip_1
-
-    def test_ip_from_dict_invalid(self):
-        """Test creating an IP object from an invalid dictionary"""
-        d = {"ip": "invalid_ip"}
-        try:
-            ip = IP.from_dict(d)
-            assert False, "Expected ValueError for invalid IP"
-        except ValueError:
-            pass
-    def test_ip_hash(self, sample_private_ip1, sample_private_ip1_copy):
-        """Test that the hash of two IP objects with the same IP is equal"""
-        ip_1, _ = sample_private_ip1
-        ip_2, _ = sample_private_ip1_copy
-        assert hash(ip_1) == hash(ip_2)
+        assert False, "Expected ValueError for invalid IP"
+    except ValueError:
+        pass
+def test_ip_hash(sample_private_ip1, sample_private_ip1_copy):
+    """Test that the hash of two IP objects with the same IP is equal"""
+    ip_1, _ = sample_private_ip1
+    ip_2, _ = sample_private_ip1_copy
+    assert hash(ip_1) == hash(ip_2)
