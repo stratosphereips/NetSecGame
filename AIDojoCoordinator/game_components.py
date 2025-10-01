627a6dc5efc31d675ef402062eb8820a13f76dc2
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-10-01 15:07:10 +0200

    Initialize empty array

--- AIDojoCoordinator/game_components.py
+++ AIDojoCoordinator/game_components.py
@@ -551,8 +551,8 @@ class GameState():
         graph_nodes = {}
         node_features = []
         controlled = []
+        edges = []
         try:
-            edges = []
             #add known nets
             for net in self.known_networks:
                 graph_nodes[net] = len(graph_nodes)
