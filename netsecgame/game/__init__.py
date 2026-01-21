05db84b710275b0cadda9b5ca0607c0be66345f8
Ondrej Lukas <ondrej.lukas95@gmail.com>
2026-01-21 11:37:21 +0100

    Add init to game module

---
+++ netsecgame/game/__init__.py
@@ -0,0 +1,9 @@
+from .coordinator import AgentServer, GameCoordinator
+from .worlds.NetSecGame import NetSecGame
+from .worlds.WhiteBoxNetSecGame import WhiteBoxNetSecGame
+__all__ = [
+    'AgentServer',
+    'GameCoordinator',
+    'NetSecGame',
+    'WhiteBoxNetSecGame'
+]
