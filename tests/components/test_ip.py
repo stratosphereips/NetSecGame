edcf6ce637f819fb82a343c2fa7fcab728399845
Ondrej Lukas <ondrej.lukas95@gmail.com>
2026-01-23 17:46:06 +0100

    remove redundant test

--- tests/components/test_ip.py
+++ tests/components/test_ip.py
@@ -87,11 +87,6 @@ def test_ip_hash(sample_private_ip1, sample_private_ip1_copy):
     ip_2, _ = sample_private_ip1_copy
     assert hash(ip_1) == hash(ip_2)
 
-def test_ip_is_private_external():
-    """Test is_private returns False for 'external' string"""
-    ip = IP("external")
-    assert ip.is_private() is False
-
 def test_ip_eq_other_type(sample_private_ip1):
     """Test equality with non-IP object"""
     ip_1, _ = sample_private_ip1
