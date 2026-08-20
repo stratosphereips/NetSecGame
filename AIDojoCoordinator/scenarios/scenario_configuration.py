8406f1aa24ed41c15b0a3b25cd2bfd5d10f01c90
Sebas Garcia <eldraco@gmail.com>
2026-08-20 12:04:07 +0200

    Small fix of the network in the scenario 0. Should not affect anything

--- AIDojoCoordinator/scenarios/scenario_configuration.py
+++ AIDojoCoordinator/scenarios/scenario_configuration.py
@@ -690,7 +690,7 @@ Internet
 '''
 internet = cyst_cfg.RouterConfig(
     interfaces=[
-        cyst_cfg.InterfaceConfig(cyst_cfg.IPAddress("213.47.23.193"), cyst_cfg.IPNetwork("213.47.23.192/26"), index=0)
+        cyst_cfg.InterfaceConfig(cyst_cfg.IPAddress("213.47.23.193"), cyst_cfg.IPNetwork("213.47.23.0/26"), index=0)
     ],
     routing_table=[
         RouteConfig(cyst_cfg.IPNetwork("192.168.0.0/16"), 0)
@@ -723,7 +723,7 @@ outside_node = cyst_cfg.NodeConfig(
         )
     ],
     traffic_processors=[],
-    interfaces=[cyst_cfg.InterfaceConfig(cyst_cfg.IPAddress("213.47.23.195"), cyst_cfg.IPNetwork("213.47.23.192/26"))],
+    interfaces=[cyst_cfg.InterfaceConfig(cyst_cfg.IPAddress("213.47.23.195"), cyst_cfg.IPNetwork("213.47.23.0/26"))],
     shell="bash",
     id="outside_node"
 )
